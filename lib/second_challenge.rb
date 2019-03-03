b072aad63cf74e71d44794d21d89395ef3e47e8b
Joy Lin <vvlnote@hotmail.com>
2019-03-03 15:46:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,57 +2,24 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => { 
-        :name => "Lord Montague",
-        :age => "53"
+      :patriarch => {
         },
       :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
         },
       :hero => {
-        :name => "Remeo",
-        :age => "15"
         },
       :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio",
-          :age => "18",
-          :attitued => "hot-headed"
-        }
         ]
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
       },
       :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
       },
       :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
       },
       :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attitue => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
         ]
       }}
 

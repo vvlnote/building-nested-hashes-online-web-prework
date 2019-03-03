8c32148293ee619ed5dcf79a00ac2b3f48062fd6
Joy Lin <vvlnote@hotmail.com>
2019-03-03 15:43:48 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,19 +3,57 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch => { 
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
+        :name => "Remeo",
+        :age => "15"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitued => "hot-headed"
+        }
+        ]
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+      :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
+      },
+      :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
+      },
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitue => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+        ]
       }}
 
   
